0627127525d775bdd71f6ed00157df8f54a72685
ARJUN VARMA <85413069+avarma57319@users.noreply.github.com>
2021-09-06 15:31:56 -0400

    Add files via upload

---
+++ 2.1 exercise_Avarma.py
@@ -0,0 +1,51 @@
+# DSC530-T301 Data Exploration and Analysis (2221-1)
+# Week 2
+# 2.1 Exercise
+# Author Arjun Varma
+# 09/06/2021
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+print("\n") # new line for spacing and formatting purposes
+# Add two numbers together
+sum = int(8+2)
+print("Sum of two numbers: ", sum)
+
+
+# Subtract a number from another number
+subtract = int(9-2)
+print("Difference of two numbers: ", subtract)
+
+
+# Multiply two numbers
+multiply = int(5 * 3)
+print("Product of two numbers: ", multiply)
+
+
+# Divide between two numbers
+divide = int(25/5)
+print("Division result of two numbers: ", divide)
+
+print("\n") # new line for spacing and formatting purposes
+# Concatenate two strings together (any words)
+string1 = "Arjun"
+string2 = "Varma"
+print("String 1:", string1)
+print("string 2:", string2)
+str = string1 + string2
+print("Concatenated strings: ", str)
+
+print("\n") # new line for spacing and formatting purposes
+# Create a list of 4 items (can be strings, numbers, both)
+my_list = [3, 5.6, "TEST", "This Works!"]
+print("Values in List: ", my_list)
+
+# Append an item to your list (again, can be a string, number)
+my_list.append("new entry")
+print("Appended additional value in List: ", my_list)
+
+print("\n") # new line for spacing and formatting purposes
+# Create a tuple with 4 items (can be strings, numbers, both)
+my_tuple = (8, 6.7, 3.4, "This is Great")
+print("Output of Tuple: ", my_tuple)
